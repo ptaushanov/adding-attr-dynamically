7ee91ea96aafaedba6e136cc0fcf3f279f92fc74
ptaushanov <pepi15032000@gmail.com>
2021-09-22 21:28:02 +0300

    feat: added attibute to product

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const price =  document.querySelector(".price").innerText
+  document.querySelector(".product").setAttribute("data-price", price)
 
 });
